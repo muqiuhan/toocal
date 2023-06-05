bfe033f430a5ba9d078a799a5dcc55705515ad8f
Muqiu Han <muqiu-han@outlook.com>
2023-06-05 17:55:25 +0800

    Add Memtbl.set tests and fixed bugs

--- lib/memtbl.ml
+++ lib/memtbl.ml
@@ -5,10 +5,10 @@ let max_size : int = 1024
 
 (** Single Record in the MemTable *)
 module Record = struct
-  (**  Each MemTableRecord holds the key and the position of the record in the ValueLog. 
-       key: The key of the record.
-       key_len: The length of the key.
-       value_loc: The location of the value in the ValueLog. *)
+  (** Each MemTableRecord holds the key and the position of the record in the ValueLog.
+      key: The key of the record.
+      key_len: The length of the key.
+      value_loc: The location of the value in the ValueLog. *)
   type t =
     { key : string;
       key_len : int;
@@ -23,9 +23,9 @@ module Record = struct
   end
 end
 
-(** MemTable of the Database. 
+(** MemTable of the Database.
     memtbl_records: Array of records sorted by key.
-    memtbl_size: The number of records filled in `records`. *)
+    memtbl_size: The number of records filled in records. *)
 type t =
   { memtbl_records : Record.t array;
     mutable memtbl_size : int }
@@ -33,69 +33,57 @@ type t =
 (** Creates a new empty MemTable. *)
 let make : unit -> t =
   fun () ->
-   { memtbl_size = 0;
-     memtbl_records =
-       Array.create ~len:max_size Record.empty }
+  { memtbl_size = 0;
+    memtbl_records =
+      Array.create ~len:max_size Record.empty }
 ;;
 
 let binary_search : t -> string -> int option =
   fun memtbl key ->
-   Array.binary_search
-     memtbl.memtbl_records
-     `First_equal_to
-     ~compare:Record.Cmp.key
-     key
+  Array.binary_search
+    memtbl.memtbl_records
+    `First_equal_to
+    ~compare:Record.Cmp.key
+    key
 ;;
 
 let insert_point : t -> string -> int =
   fun memtbl key ->
-   match
-     Array.binary_search
-       memtbl.memtbl_records
-       `First_greater_than_or_equal_to
-       ~compare:Record.Cmp.key
-       key
-   with
-   | None -> 0
-   | Some index -> index
+  match
+    Array.binary_search
+      memtbl.memtbl_records
+      `First_greater_than_or_equal_to
+      ~compare:Record.Cmp.key
+      key
+  with
+  | None -> 0
+  | Some index -> index
 ;;
 
-(** Gets a MemTableRecord from a MemTable by key. 
+(** Gets a MemTableRecord from a MemTable by key.
     This function will return None if none of the records in the MemTable.
     This function uses binary search for a runtime of O(log(n)). *)
 let get : t -> string -> Record.t option =
   fun memtbl key ->
-   Option.map (binary_search memtbl key) ~f:(fun index ->
-     memtbl.memtbl_records.(index))
+  Option.map (binary_search memtbl key) ~f:(fun index ->
+      memtbl.memtbl_records.(index))
 ;;
 
 (** Sets a key-value pair in a MemTable.
     This function uses binary search for a runtime of O(log(n)) *)
 let set : t -> string -> int64 -> unit =
   fun memtbl key value_loc ->
-   let insert_index = insert_point memtbl key in
-       begin
-         match binary_search memtbl key with
-         | None ->
-           Array.blit
-             ~src:memtbl.memtbl_records
-             ~src_pos:insert_index
-             ~dst:memtbl.memtbl_records
-             ~dst_pos:(insert_index + 1)
-             ~len:(memtbl.memtbl_size - insert_index - 1);
-           memtbl.memtbl_records.(insert_index)
-             <- Record.
-                  { key;
-                    value_loc;
-                    key_len = String.length key }
-         | Some _ ->
-           memtbl.memtbl_records.(insert_index)
-             <- Record.
-                  { key;
-                    value_loc;
-                    key_len = String.length key }
-       end;
-       memtbl.memtbl_size <- memtbl.memtbl_size + 1
+  let insert_index = insert_point memtbl key in
+  Array.blit
+    ~src:memtbl.memtbl_records
+    ~src_pos:insert_index
+    ~dst:memtbl.memtbl_records
+    ~dst_pos:(insert_index + 1)
+    ~len:(memtbl.memtbl_size - insert_index);
+  memtbl.memtbl_records.(insert_index)
+  <- Record.
+     {key; value_loc; key_len = String.length key};
+  memtbl.memtbl_size <- memtbl.memtbl_size + 1
 ;;
 
 (** Deletes a record from a MemTable.
--- test/test_memtbl.ml
+++ test/test_memtbl.ml
@@ -3,8 +3,8 @@ open Wiscodb
 let test_set () =
   let memtbl = Memtbl.make () in
       Memtbl.set memtbl "aaa" Int64.zero;
-      match memtbl.memtbl_records with
-      | [|record|] ->
+      match memtbl.memtbl_records |> Array.to_list with
+      | record :: _ ->
         Alcotest.(check string) "same key" "aaa" record.key;
         Alcotest.(check int) "same key_len" 3 record.key_len;
         Alcotest.(check int64)
@@ -14,7 +14,80 @@ let test_set () =
       | _ -> Alcotest.fail "test_set"
 ;;
 
+let test_set_multi_elements () =
+  let memtbl = Memtbl.make () in
+      Memtbl.set memtbl "aaa" Int64.zero;
+      Memtbl.set memtbl "bbb" Int64.zero;
+      Memtbl.set memtbl "ccc" Int64.zero;
+      match memtbl.memtbl_records |> Array.to_list with
+      | record1 :: record2 :: record3 :: _ ->
+        Alcotest.(check string) "same key" "ccc" record1.key;
+        Alcotest.(check int)
+          "same key_len"
+          3
+          record1.key_len;
+        Alcotest.(check int64)
+          "same value_loc"
+          Int64.zero
+          record1.value_loc;
+        Alcotest.(check string) "same key" "bbb" record2.key;
+        Alcotest.(check int)
+          "same key_len"
+          3
+          record2.key_len;
+        Alcotest.(check int64)
+          "same value_loc"
+          Int64.zero
+          record2.value_loc;
+        Alcotest.(check string) "same key" "aaa" record3.key;
+        Alcotest.(check int)
+          "same key_len"
+          3
+          record3.key_len;
+        Alcotest.(check int64)
+          "same value_loc"
+          Int64.zero
+          record3.value_loc
+      | _ -> Alcotest.fail "test_set"
+;;
+
+let test_set_overwrite () =
+  let memtbl = Memtbl.make () in
+      Memtbl.set memtbl "aaa" Int64.zero;
+      Memtbl.set memtbl "aaa" Int64.one;
+      match memtbl.memtbl_records |> Array.to_list with
+      | record1 :: record2 :: _ ->
+        Alcotest.(check string) "same key" "aaa" record1.key;
+        Alcotest.(check int)
+          "same key_len"
+          3
+          record1.key_len;
+        Alcotest.(check int64)
+          "same value_loc"
+          Int64.one
+          record1.value_loc;
+        Alcotest.(check string) "same key" "aaa" record2.key;
+        Alcotest.(check int)
+          "same key_len"
+          3
+          record2.key_len;
+        Alcotest.(check int64)
+          "same value_loc"
+          Int64.zero
+          record2.value_loc
+      | _ -> Alcotest.fail "test_set"
+;;
+
 let tests =
   Alcotest.(
-    "Memtbl", [test_case "Memtbl.set" `Quick test_set])
+    ( "Memtbl",
+      [ test_case "Memtbl.set" `Quick test_set;
+        test_case
+          "Memtbl.set muti elements"
+          `Quick
+          test_set_multi_elements;
+        test_case
+          "Memtbl.set overwrite"
+          `Quick
+          test_set_overwrite ] ))
 ;;
