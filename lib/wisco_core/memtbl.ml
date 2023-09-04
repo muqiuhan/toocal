19196e18bb8c34a1606b9e7ae0dbe7f937983278
Muqiu Han <muqiu-han@outlook.com>
2023-09-04 10:29:43 +0800

    Add: Memtbl implementation

--- lib/wisco_core/memtbl.ml
+++ lib/wisco_core/memtbl.ml
@@ -14,15 +14,33 @@
  * limitations under the License.
  *)
 
-(** Each MemTableRecord holds the key and the position of the record in the ValueLog.
-    [key] The key of the record
-    [value_loc] the location of the key in the value log. *)
-type memtbl_record =
-  { key : string;
-    value_loc : int }
+open Core
+open Wisco_utils
+
+module Record = struct
+  (** Each MemTableRecord holds the key and the position of the record in the ValueLog.
+      [key] The key of the record
+      [value_loc] the location of the key in the value log. *)
+  type record =
+    { key : string;
+      value_loc : int }
+
+  let compare (r : record) (c : record) : int = compare_string r.key c.key
+
+  let to_string {key : string; value_loc : int} : string =
+    Format.sprintf "{key = %s; value_loc: %d}" key value_loc
+  ;;
+
+  type t = record
+end
+
+include Skiplist.Make (Record)
 
 (** In-memory table of the records that have been modified most recently.
     At any given time, there is only one active MemTable in the database engine.
     The MemTable is always the first store to be searched when a key-value pair is requested.
     [records] Array of the records sort by key *)
-type memtbl = {records : memtbl_record array}
+type memtbl = unit t
+
+(** Creates a new empty memtbl. *)
+let make () : memtbl = create ()
