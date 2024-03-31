19ebca7b0255c36e817a257e005ba393daf45d7d
Muqiu Han <muqiu-han@outlook.com>
2024-03-31 21:17:52 +0800

    [dal]: add freelist

--- src/main/scala/com/muqiuhan/toocal/dal/FreeList.scala
+++ src/main/scala/com/muqiuhan/toocal/dal/FreeList.scala
@@ -28,42 +28,20 @@
 
 package com.muqiuhan.toocal.dal
 
-import java.io.{File, RandomAccessFile}
+import scala.collection.mutable
 
-class DataAccessLayer(path: String, pageSize: Int):
-  private val file     = DataAccessLayer.openDataBaseFile(path)
-  private val freeList = FreeList()
+class FreeList:
+  private var maxPage: PageNum                     = FreeList.META_PAGE
+  private val releasePages: mutable.Stack[PageNum] = mutable.Stack()
 
-  def close(): Unit = file.close()
-
-  def allocateEmptyPage(): Page = Page(num = pageSize, data = new Array[Byte](pageSize))
-
-  def readPage(pageNum: PageNum): Page =
-    val page = allocateEmptyPage()
-
-    file.read(page.data, pageNum.asInstanceOf[Int] * pageSize, page.data.length) match
-      case -1 =>
-        throw ReadPageException("There is no more data because the end of the database file has been reached.")
-      case bytes: Int if bytes != page.num =>
-        throw ReadPageException(s"Incomplete read, successfully read $bytes bytes")
-      case _ =>
-        page
-
-  def writePage(page: Page): Unit = file.write(page.data, page.num.asInstanceOf[Int] * pageSize, page.data.length)
-
-private object DataAccessLayer:
-  def openDataBaseFile(path: String): RandomAccessFile =
-    val file = File(path)
-
-    if !file.exists() then
-      file.createNewFile()
+  def getNextPage: PageNum =
+    if releasePages.nonEmpty then
+      releasePages.pop()
     else
-      if !file.canRead then
-        DataBaseFileException("The database file does not have read permission!")
-      if !file.canWrite then
-        DataBaseFileException("The database file does not have write permission!")
+      maxPage = maxPage + 1
+      maxPage
 
-    RandomAccessFile(path, "rw")
+  def releasePage(page: PageNum): Unit = releasePages.push(page)
 
-class DataBaseFileException(msg: String) extends RuntimeException(msg)
-class ReadPageException(msg: String)     extends RuntimeException(msg)
+private object FreeList:
+  private val META_PAGE: PageNum = 0L
