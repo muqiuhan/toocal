2e3fb9c5aba070e7a5fb0d3028b10b7dbac9181a
Muqiu Han <muqiu-han@outlook.com>
2024-04-03 11:42:59 +0800

    [dal]: add the node model

--- src/main/scala/com/muqiuhan/toocal/dal/DataAccessLayer.scala
+++ src/main/scala/com/muqiuhan/toocal/dal/DataAccessLayer.scala
@@ -1,42 +1,51 @@
-// Copyright (c) 2023 Muqiu Han
-//
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without modification,
-// are permitted provided that the following conditions are met:
-//
-//     * Redistributions of source code must retain the above copyright notice,
-//       this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above copyright notice,
-//       this list of conditions and the following disclaimer in the documentation
-//       and/or other materials provided with the distribution.
-//     * Neither the name of Terifo nor the names of its contributors
-//       may be used to endorse or promote products derived from this software
-//       without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/* Copyright (c) 2023 Muqiu Han
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright notice,
+ *       this list of conditions and the following disclaimer in the documentation
+ *       and/or other materials provided with the distribution.
+ *     * Neither the name of Terifo nor the names of its contributors
+ *       may be used to endorse or promote products derived from this software
+ *       without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 package com.muqiuhan.toocal.dal
 
-import java.io.{ File, RandomAccessFile }
+import java.io.{File, RandomAccessFile}
 
+/** Data Access Layer (DAL) handles all disk operations and how data is
+  * organized on the disk. It’s responsible for managing the underlying data
+  * structure, writing the database pages to the disk, and reclaiming free pages
+  * to avoid fragmentation.
+  */
 class DataAccessLayer(path: String, pageSize: Int):
   private val (file, databaseFileStatus) =
     DataAccessLayer.openDataBaseFile(path)
 
   private var freeList = FreeList()
-  private var meta = Meta()
+  private var meta     = Meta()
 
+  /* Initialization logic,
+   * if the database file exists, load it,
+   * otherwise create a new database file and write freelist and meta */
   databaseFileStatus match
     case DataBaseFileStatus.Exist =>
       meta = readMeta()
@@ -57,9 +66,13 @@ class DataAccessLayer(path: String, pageSize: Int):
     file.seek(pageNum.toInt * pageSize)
     file.read(page.data) match
       case -1 =>
-        throw ReadPageException("There is no more data because the end of the database file has been reached.")
+        throw ReadPageException(
+          "There is no more data because the end of the database file has been reached."
+        )
       case bytes: Int if bytes != page.num =>
-        throw ReadPageException(s"Incomplete read, successfully read $bytes bytes")
+        throw ReadPageException(
+          s"Incomplete read, successfully read $bytes bytes"
+        )
       case _ =>
         page
 
@@ -103,8 +116,14 @@ private object DataAccessLayer:
       file.createNewFile()
       (RandomAccessFile(path, "rw"), DataBaseFileStatus.NotExist)
     else
-      if !file.canRead then DataBaseFileException("The database file does not have read permission!")
-      if !file.canWrite then DataBaseFileException("The database file does not have write permission!")
+      if !file.canRead then
+        DataBaseFileException(
+          "The database file does not have read permission!"
+        )
+      if !file.canWrite then
+        DataBaseFileException(
+          "The database file does not have write permission!"
+        )
       (RandomAccessFile(path, "rw"), DataBaseFileStatus.Exist)
 
 private enum DataBaseFileStatus:
@@ -112,4 +131,4 @@ private enum DataBaseFileStatus:
   case NotExist
 
 class DataBaseFileException(msg: String) extends RuntimeException(msg)
-class ReadPageException(msg: String) extends RuntimeException(msg)
+class ReadPageException(msg: String)     extends RuntimeException(msg)
--- src/main/scala/com/muqiuhan/toocal/dal/Meta.scala
+++ src/main/scala/com/muqiuhan/toocal/dal/Meta.scala
@@ -1,12 +1,54 @@
+/* Copyright (c) 2023 Muqiu Han
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright notice,
+ *       this list of conditions and the following disclaimer in the documentation
+ *       and/or other materials provided with the distribution.
+ *     * Neither the name of Terifo nor the names of its contributors
+ *       may be used to endorse or promote products derived from this software
+ *       without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package com.muqiuhan.toocal.dal
 
 import java.nio.ByteBuffer
 
-case class Meta(var freeListPage: PageNum = 1):
-  def serialize(buffer: Array[Byte]): Unit =
-    ByteBuffer.allocate(Page.PAGE_NUM_SIZE).putLong(freeListPage).array.copyToArray(buffer)
+/** Meta is a special Page whose PageNum is always 0. It is the first page of
+  * the database and is used to store metadata in the database, such as the
+  * location of FreeList and other information.
+  */
+case class Meta(
+    /** the page of FreeList should be on the next page of Meta */
+    var freeListPage: PageNum = 1
+):
 
-  def deserialize(buffer: Array[Byte]): Unit = freeListPage = ByteBuffer.wrap(buffer).getLong()
+  def serialize(buffer: Array[Byte]): Unit =
+    ByteBuffer
+      .allocate(Page.PAGE_NUM_SIZE)
+      .putLong(freeListPage)
+      .array
+      .copyToArray(buffer)
+
+  def deserialize(buffer: Array[Byte]): Unit = freeListPage =
+    ByteBuffer.wrap(buffer).getLong()
 
 object Meta:
   val PAGE_NUM: Int = 0
