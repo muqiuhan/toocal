ddef2387e215b150bb23201f4b33708995128602
Muqiu Han <muqiu-han@outlook.com>
2024-08-12 17:31:34 +0800

    [core]: Add node operations (find)
      * Note: The `__find_key_helper` and find function cannot call by reference
      * Related issue: #1

--- src/utils.rs
+++ src/utils.rs
@@ -1,27 +1,4 @@
 pub mod page_size {
-    //! This crate provides an easy, fast, cross-platform way to retrieve the
-    //! memory page size of the current system.
-    //!
-    //! Modern hardware and software tend to load data into RAM (and transfer data
-    //! from RAM to disk) in discrete chunk called pages. This crate provides a
-    //! helper method to retrieve the size in bytes of these pages. Since the page
-    //! size *should not* change during execution, this crate will cache the result
-    //! after it has been called once.
-    //!
-    //! To make this crate useful for writing memory allocators, it does not require
-    //! (but can use) the Rust standard library.
-    //!
-    //! Since Windows addresses sometimes have to correspond with an allocation
-    //! granularity that does not always match the size of the page, I have included
-    //! a method to retrieve that as well.
-    //!
-    //! # Example
-    //!
-    //! ```rust
-    //! extern crate page_size;
-    //! println!("{}", page_size::get());
-    //! ```
-
     extern crate std;
     use std::sync::Once;
 
@@ -43,20 +20,6 @@ pub mod page_size {
         get_helper()
     }
 
-    /// This function retrieves the system's memory allocation granularity.
-    ///
-    /// # Example
-    ///
-    /// ```rust
-    /// extern crate page_size;
-    /// println!("{}", page_size::get_granularity());
-    /// ```
-    pub fn get_granularity() -> usize {
-        get_granularity_helper()
-    }
-
-    // Unix Section
-
     #[cfg(all(unix))]
     #[inline]
     fn get_helper() -> usize {
@@ -67,14 +30,6 @@ pub mod page_size {
             INIT.call_once(|| PAGE_SIZE = unix::get());
             PAGE_SIZE
         }
-    }
-
-    // Unix does not have a specific allocation granularity.
-    // The page size works well.
-    #[cfg(unix)]
-    #[inline]
-    fn get_granularity_helper() -> usize {
-        get_helper()
     }
 
     #[cfg(unix)]
@@ -96,7 +51,7 @@ pub mod page_size {
         any(target_arch = "wasm32", target_arch = "wasm64")
     ))]
     #[inline]
-    fn get_granularity_helper() -> usize {
+    fn get() -> usize {
         // <https://webassembly.github.io/spec/core/exec/runtime.html#page-size>
         65536
     }
@@ -112,18 +67,6 @@ pub mod page_size {
         unsafe {
             INIT.call_once(|| PAGE_SIZE = windows::get());
             PAGE_SIZE
-        }
-    }
-
-    #[cfg(all(windows))]
-    #[inline]
-    fn get_granularity_helper() -> usize {
-        static GRINIT: Once = Once::new();
-        static mut GRANULARITY: usize = 0;
-
-        unsafe {
-            GRINIT.call_once(|| GRANULARITY = windows::get_granularity());
-            GRANULARITY
         }
     }
 
@@ -146,16 +89,6 @@ pub mod page_size {
                 info.dwPageSize as usize
             }
         }
-
-        #[inline]
-        pub fn get_granularity() -> usize {
-            unsafe {
-                let mut info: SYSTEM_INFO = mem::zeroed();
-                GetSystemInfo(&mut info as LPSYSTEM_INFO);
-
-                info.dwAllocationGranularity as usize
-            }
-        }
     }
 
     // Stub Section
@@ -174,12 +107,6 @@ pub mod page_size {
         fn test_get() {
             #[allow(unused_variables)]
             let page_size = get();
-        }
-
-        #[test]
-        fn test_get_granularity() {
-            #[allow(unused_variables)]
-            let granularity = get_granularity();
         }
     }
 }
