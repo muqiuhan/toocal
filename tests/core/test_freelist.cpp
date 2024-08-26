3c8c0662a0105e759a6dd2d0d068fda970a90223
Muqiu Han <muqiu-han@outlook.com>
2024-08-26 16:43:19 +0800

    [core]: implemented freelist.
      * write unit tests with doctest.
      * #2

--- tests/core/test_freelist.cpp
+++ tests/core/test_freelist.cpp
@@ -1,43 +1,26 @@
-#include <algorithm>
-#include <cstddef>
-#include <cstdint>
-#include <tl/expected.hpp>
-#include <vector>
-
+#include <doctest/doctest.h>
 #include "freelist.h"
-#include "errors.hpp"
 #include "page.h"
-#include "spdlog/fmt/bundled/core.h"
 
 namespace toocal::core::freelist::tests
 {
-  static auto test_serializer(std::nullptr_t) noexcept
-    -> tl::expected<std::nullptr_t, core::errors::Error>
+  TEST_CASE("serializer")
   {
     const auto buffer =
       types::Serializer<Freelist>::serialize(Freelist{1, std::vector<page::Page_num>(10)})
         .value();
+
     const auto freelist = types::Serializer<Freelist>::deserialize(buffer);
 
-    if (freelist->max_page != 1)
-      return tl::unexpected(__error(
-        fmt::format("freelist->max_page == {}, excepted {}", freelist->max_page, 1)));
+    CHECK_EQ(freelist->max_page, 1);
+    CHECK_EQ(freelist->released_pages.size(), 10);
+  }
 
-    if (freelist->released_pages.size() != 10)
-      return tl::unexpected(__error(fmt::format(
-        "freelist->released_pages.size() == {}, excepted {}",
-        freelist->released_pages.size(),
-        10)));
+  TEST_CASE("get_next_page")
+  {
+    auto freelist = Freelist{};
 
-    return nullptr;
+    CHECK_EQ(freelist.get_next_page(), 1);
+    CHECK_EQ(freelist.max_page, 1);
   }
 } // namespace toocal::core::freelist::tests
-
-auto main(int argc, char *argv[]) -> int
-{
-  using namespace toocal::core::freelist::tests;
-
-  test_serializer(nullptr).or_else([](const auto &err) { err.panic(); });
-
-  return 0;
-}
